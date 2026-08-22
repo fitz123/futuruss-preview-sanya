39b74346b5057057bd43891ded4ae81f94e571c0
fitz123 <10243861+fitz123@users.noreply.github.com>
2026-08-22 15:08:13 +0300

    deploy: publish compiled FUTURUSS preview

---
+++ _next/static/ekmE9_uvrnx9vG7jJfqKH/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Facademy\u002F[material]\u002F[article]","\u002Fmanagers\u002F[slug]","\u002Fproducts\u002F[product]","\u002Fproducts\u002F[product]\u002Fmaterial\u002F[material]","\u002Fproducts\u002F[product]\u002Fmaterial\u002F[material]\u002Fsystem\u002F[system]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
